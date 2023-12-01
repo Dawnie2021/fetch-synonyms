6e7604ee41098af5f9cba77b02d0f55fd22dd020
Seven <phoenixpyra7@gmail.com>
2023-11-30 21:40:31 -0600

    I only added a few semi-colons and formated both js and hhtml. -sjn

--- assets/script.js
+++ assets/script.js
@@ -9,30 +9,31 @@ var result3 = document.getElementById("result3");
 apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/<hello>`
 
 function searchWord() {
-    var word = wordInput.value 
-    fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/` + word)
-.then(function(response) {
-return response.json();
+    var word = wordInput.value
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/` + word);
+        .then(function (response) {
+        return response.json();
+    }
+    )
+        .then(function (data) {
+            console.log(data);
+            console.log(data[0].synonyms);
+            result1.innerHTML = data[0].meanings[0].synonyms[0];
+            result2.innerHTML = data[0].meanings[0].synonyms[1];
+            result3.innerHTML = data[0].meanings[0].synonyms[2];
+        }
+        )
 }
-)
-.then(function (data) {
-   console.log(data);
-   console.log(data[0].synonyms);
-   result1.innerHTML = data[0].meanings[0].synonyms[0];
-   result2.innerHTML = data[0].meanings[0].synonyms[1];
-   result3.innerHTML = data[0].meanings[0].synonyms[2];
-}
-)}
-searchButton.addEventListener ("click", function(event){
-    event.preventDefault()
+searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
     searchWord();
 });
 
 function displaySynonyms(data) {
-   displayResults[2]
+    displayResults[2];
 }
 
 function ReadAloud() {
     var synonyms = synonyms.value
-    fetch (`https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/` + synonyms)
-    }
+    fetch(`https://mdn.github.io/dom-examples/web-speech-api/speak-easy-synthesis/` + synonyms);
+}
