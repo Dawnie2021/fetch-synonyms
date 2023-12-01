1b586718f09dd0ecb6d5324780bbd17181c885ca
JacksonByrd7 <jackson.byrd7@gmail.com>
2023-11-30 19:09:36 -0500

    wrapped html function in div, created var arrays in js

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var wordInput = document.getElementById("word");
+var searchButton = document.getElementById("fetch-button");
+var synonyms = document.getElementById("synonyms");
+
+apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/<word>`
+
+for (let i = 0; i <data.list.length i +=3) {
+    
+}
