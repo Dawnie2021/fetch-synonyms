2dd569cb5d8736c0eb83bb5d2268bd4685e25cd8
JacksonByrd7 <jackson.byrd7@gmail.com>
2023-11-30 21:25:21 -0500

    got synonyms to print to page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var wordInput = document.getElementById("word");
 var searchButton = document.getElementById("fetch-button");
 var synonyms = document.getElementById("synonyms");
+var result1 = document.getElementById("result1");
+var result2 = document.getElementById("result2");
+var result3 = document.getElementById("result3");
 
 
 apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/<hello>`
@@ -13,8 +16,11 @@ return response.json();
 }
 )
 .then(function (data) {
-   // getSynonyms("synonyms");
    console.log(data);
+   console.log(data[0].synonyms);
+   result1.innerHTML = data[0].meanings[0].synonyms[0];
+   result2.innerHTML = data[0].meanings[0].synonyms[1];
+   result3.innerHTML = data[0].meanings[0].synonyms[2];
 }
 )}
 searchButton.addEventListener ("click", function(event){
@@ -22,8 +28,8 @@ searchButton.addEventListener ("click", function(event){
     searchWord();
 });
 
-function showSynonyms() {
-    
+function displaySynonyms(data) {
+   displayResults[2]
 }
 
 function ReadAloud() {
