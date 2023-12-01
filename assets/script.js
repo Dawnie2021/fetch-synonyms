039a6f15d2b446a893eafba41c792942b2e80e08
JacksonByrd7 <jackson.byrd7@gmail.com>
2023-11-30 19:50:43 -0500

    reworked functions

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,22 @@ var wordInput = document.getElementById("word");
 var searchButton = document.getElementById("fetch-button");
 var synonyms = document.getElementById("synonyms");
 
-apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/<word>`
 
-fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/<word>`)
-.then(function(response){
+apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/<hello>`
+
+function searchWord() {
+    var word = wordInput.value 
+    fetch (`https://api.dictionaryapi.dev/api/v2/entries/en/` + word)
+.then(function(response) {
 return response.json();
 }
 )
-.then(function (data){
-    getSynonyms("word");
+.then(function (data) {
+   // getSynonyms("synonyms");
+   console.log(data);
 }
-)
+)}
+searchButton.addEventListener ("click", function(event){
+    event.preventDefault()
+    searchWord();
+})
